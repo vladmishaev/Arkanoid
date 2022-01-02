245ebb2d68c840a1903087bb5cb64420f5dbe799
vladmishaev <vlad.mishaev.1998@gmail.com>
2022-01-02 19:03:48 +0300

    'refactor'

--- game.js
+++ game.js
@@ -1,12 +1,41 @@
 const GAME = {
-    startGame() {
-        this.ctx = document.querySelector('#canvasGame').getContext('2d');
-        const background = new Image();
-        background.src = 'img/background.png';
-        window.requestAnimationFrame(() => {
-            this.ctx.drawImage(background, 0, 0);
+    ctx: null,
+    sprites: {
+        background: {
+            src: 'background.png',
+        },
+        ball: {
+            src: 'ball.png',
+        },
+        platform: {
+            src: 'platform.png',
 
+        }
+    },
+
+    init() {
+        this.ctx = document.querySelector('#canvasGame').getContext('2d');
+    },
+    preload() {
+        for (const Key in this.sprites) {
+            let {src} = this.sprites[Key];
+            const img = new Image();
+            img.src = `img/${src}`;
+            img.addEventListener('load', () => {
+                this.renderCanvas(img, 0, 0)
+            })
+
+
+        }
+    },
+    renderCanvas(element, y, x) {
+        window.requestAnimationFrame(() => {
+            this.ctx.drawImage(element, y, x);
         });
+    },
+    startGame() {
+        this.init();
+        this.preload();
 
     }
 }
